c345cbcf66d5a103da0de1299e784866efee4e21
harryge00 <harryge00@gmail.com>
2016-09-14 00:38:02 +0800

    query and writes

--- main.go
+++ main.go
@@ -4,20 +4,36 @@ import (
   "fmt"
   "bytes"
   "os"
+  _url "net/url"
 	"io/ioutil"
 	"net/http"
 )
 
-func getJsonBody() string{
+func getBody() string{
   return "not implemented"
 }
 
-func doRequest() {
-  url := "http://" + os.Getenv("DB")
-  fmt.Println("URL:>", url)
+func query() {
+  url := "http://" + os.Getenv("URL") + "/query"
+  v := _url.Values{}
+  v.Set("db", os.Getenv("DB"))
+  v.Add("q", "SELECT \"value\" FROM \"cpu_load_short\" WHERE \"region\"='us-west';SELECT count(\"value\") FROM \"cpu_load_short\" WHERE \"region\"='us-west'")
+  client := &http.Client{}
+  resp, err := client.PostForm(url, v)
+  if err != nil {
+      panic(err)
+  }
+  fmt.Println("response Status:", resp.Status)
+  fmt.Println("response Headers:", resp.Header)
+  body, _ := ioutil.ReadAll(resp.Body)
+  fmt.Println("response Body:", string(body))
+}
 
-  var jsonStr = []byte(getJsonBody())
-  req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonStr))
+func write() {
+  url := "http://" + os.Getenv("URL") + "/write?db=" + os.Getenv("DB")
+  fmt.Println("URL:>", url)
+  var bodyBytes = []byte("cpu_load_short,host=server01,region=us-east value=0.89 1434055562000000000")
+  req, err := http.NewRequest("POST", url, bytes.NewBuffer(bodyBytes))
   req.Header.Set("Content-Type", "application/json")
 
   client := &http.Client{}
@@ -32,6 +48,7 @@ func doRequest() {
   body, _ := ioutil.ReadAll(resp.Body)
   fmt.Println("response Body:", string(body))
 }
+
 func main() {
-  doRequest()
+  write()
 }
